34ac9bc7da440e65ec5b48247b65460c7429b0b5
puvvadasaikiran <30699361+puvvadasaikiran@users.noreply.github.com>
2020-04-15 16:01:00 +0530

    donner

--- lib/main.dart
+++ lib/main.dart
@@ -1,111 +1,153 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'dart:async';
+import 'dart:convert';
+import 'package:http/http.dart';
+import 'package:html/parser.dart' as parser;
+import 'package:html/dom.dart' as dom;
 
-void main() => runApp(MyApp());
+void main() => runApp(MaterialApp(
+      darkTheme: ThemeData.dark(),
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    ));
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class HomePage extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
+  _HomePageState createState() => _HomePageState();
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+class _HomePageState extends State<HomePage> {
+  StreamSubscription<QuerySnapshot> subscription;
+  List<DocumentSnapshot> wallpapersList;
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+  final CollectionReference collectionReference =
+      Firestore.instance.collection("wallpapers");
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+  void initState() {
+    initiate();
+    subscription = collectionReference.snapshots().listen((datasnapshop) {
+      setState(() {
+        wallpapersList = datasnapshop.documents;
+      });
     });
+    // TODO: implement initState
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    subscription?.cancel();
+    // TODO: implement dispose
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
+      body: Container(
+        color: Colors.black87,
+        child: SafeArea(
+          maintainBottomViewPadding: true,
+          child: Column(
+            children: <Widget>[
+              Container(
+                height: 50,
+                child: Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 15.0, vertical: 2),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Text(
+                        'WallPostr',
+                        style: TextStyle(
+                          fontSize: 25,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                        ),
+                      ),
+                      IconButton(
+                          icon: Icon(
+                            Icons.more_horiz,
+                            color: Colors.white,
+                          ),
+                          onPressed: () {
+                            print('Hell');
+                          })
+                    ],
+                  ),
+                ),
+              ),
+              wallpapersList != null
+                  ? Container(
+                      height: MediaQuery.of(context).size.height - 74,
+                      padding: const EdgeInsets.all(20.0),
+                      child: GridView.builder(
+                        semanticChildCount: 113,
+                        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                          crossAxisCount: 2,
+                          crossAxisSpacing: 15,
+                          mainAxisSpacing: 15,
+                          childAspectRatio: 2 / 2.1,
+                        ),
+                        physics: BouncingScrollPhysics(),
+                        scrollDirection: Axis.vertical,
+                        itemBuilder: (
+                          context,
+                          index,
+                        ) {
+                          return Container(
+                            // height: 300,
+                            // margin: EdgeInsets.only(bottom: 30),
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(15),
+                              image: DecorationImage(
+                                  image: NetworkImage(
+                                      wallpapersList[index].data['url']),
+                                  fit: BoxFit.cover),
+                            ),
+                          );
+                        },
+                        itemCount: wallpapersList.length,
+                      ),
+                    )
+                  : Container(
+                      child: Center(
+                        child: Text('No '),
+                      ),
+                    ),
+            ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+
+  void initiate() async {
+    // Make API call to Hackernews homepage
+    
+
+    Response response = await client.get(
+      'https://irobot.in/request.aspx?id=order000001030',
+    );
+    print(response.body);
+
+    // Use html parser
+    // var document = parser.parse(response.body);
+    // List<Element> links = document.querySelectorAll('td.title > a.storylink');
+    // List<Map<String, dynamic>> linkMap = [];
+
+    // for (var link in links) {
+    //   linkMap.add({
+    //     'title': link.text,
+    //     'href': link.attributes['href'],
+    //   });
+    // }
+
+    // print(json.encode(linkMap));
   }
 }
